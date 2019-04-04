ec7e5b6dcdeeda55afa444839b7926a718c78e50
Aini <Jontorek@gmail.com>
2019-04-04 21:54:18 +0200

    Add main file, update Maven settings

---
+++ src/main/java/com/mcc/mcC.java
@@ -0,0 +1,13 @@
+public class mcC {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            // error
+            return;
+        }
+        
+        String fileName = args[0];
+
+        // parse file
+        // generate bytecode
+    }
+}
